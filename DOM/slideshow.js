784c541904e031ab73ec9155f14f3eb97f52f979
Sorawit <sorawit.nu@ku.th>
2021-05-10 04:50:14 +0700

    fully function card slider

--- DOM/slideshow.js
+++ DOM/slideshow.js
@@ -1,4 +1,4 @@
-let card_list = document.getElementsByClassName("card")
+let cardHTMLCollection = document.getElementsByClassName("card")
 
 let button_left = document.getElementById("js-button1")
 let button_right = document.getElementById("js-button2")
@@ -7,10 +7,12 @@ let debug = document.getElementById("debug")
 
 let card_position = [];
 
+let midPointer = 0
+
 
 function even(num) {
     if(num%2===0){
-return true
+    return true
     }
     return false
 }
@@ -21,28 +23,50 @@ debug.onclick = () => {
     console.log(card_position)
 }
 
-button_right.onclick = () => {
-    for(let i=0; i<card_list.length; i++){
-        card_position[i] = card_position[i] + 315;
+function update() {
+    if(even(cardHTMLCollection.length)){
+        for(let i=0; i<cardHTMLCollection.length; i++){
+            cardHTMLCollection[i].style.left = `calc(50% + 25px + ${card_position[i]}px)`
+        }
+    }else{
+        for(let i=0; i<cardHTMLCollection.length; i++){
+            cardHTMLCollection[i].style.left = `calc(50% - 132px + ${card_position[i]}px)`
+        }
     }
-    for(let i=0; i<card_list.length; i++){
-        card_list[i].style.left = `calc(50% - 132px + ${card_position[i]}px)`
+}
+
+button_right.onclick = () => {
+    if(midPointer>0){
+        for(let i=0; i<cardHTMLCollection.length; i++){
+            card_position[i] = card_position[i] + 315;
+        }
+        cardHTMLCollection[midPointer].style.filter = `blur(2px)`
+        cardHTMLCollection[midPointer].style.transform = 'scale(0.8)'
+        midPointer--
+        cardHTMLCollection[midPointer].style.filter = `drop-shadow(0px 4px 4px rgba(0, 0, 0, 0.25))`
+        cardHTMLCollection[midPointer].style.transform = 'scale(1)'
+        update()
     }
 }
 
 button_left.onclick = () => {
-    for(let i=0; i<card_list.length; i++){
-        card_position[i] = card_position[i] - 315;
-    }
-    for(let i=0; i<card_list.length; i++){
-        card_list[i].style.left = `calc(50% - 132px + ${card_position[i]}px)`
+    if(midPointer<cardHTMLCollection.length-1){
+        for(let i=0; i<cardHTMLCollection.length; i++){
+            card_position[i] = card_position[i] - 315;
+        }
+        cardHTMLCollection[midPointer].style.filter = `blur(2px)`
+        cardHTMLCollection[midPointer].style.transform = 'scale(0.8)'
+        midPointer++
+        cardHTMLCollection[midPointer].style.filter = `drop-shadow(0px 4px 4px rgba(0, 0, 0, 0.25))`
+        cardHTMLCollection[midPointer].style.transform = 'scale(1)'
+        update()
     }
 }
 
 
 function check() {
-    for(let i=0; i<card_list.length; i++){
-    console.log(card_list[i].style.left);
+    for(let i=0; i<cardHTMLCollection.length; i++){
+    console.log(cardHTMLCollection[i].style.left);
     }
 }
 
@@ -55,49 +79,50 @@ function main(){
     /* Check Card number that have odd or even */
     let counter = 1
 
-    if(card_list.length == 1){
+    if(cardHTMLCollection.length == 1){
         console.log("later")
-    }else if(even(card_list.length)){
-        let midRight = card_list.length/2
+    }else if(even(cardHTMLCollection.length)){
+        let midRight = cardHTMLCollection.length/2
+        midPointer = midRight
         let midLeft = midRight - 1
-        card_position.push(315)
+        card_position.push(-315)
         card_position.push(0)
-
-        card_list[midLeft].style.left = `calc(50% + 25px - 315px)`
-        card_list[midRight].style.left = `calc(50% + 25px + 0px)`
-
 
         // left side position assign
         counter = 2
         for(let i=0; i<midLeft; i++){
-            card_position.unshift(315*(counter))
+            card_position.unshift(-315*(counter))
             counter++;
         }
 
         // Right side position assign
         counter = 1;
-        for(let i=midRight + 1; i<card_list.length; i++){
+        for(let i=midRight + 1; i<cardHTMLCollection.length; i++){
             card_position.push(315*(counter))
             counter++;
         }
 
         console.log(card_position)
 
-        for(let i=0; i<midLeft; i++){
-            card_list[i].style.left = `calc(50% + 25px - ${card_position[i]}px)`
-            //console.log(card_list[i].style.left)
+        for(let i=0; i<=midLeft; i++){
+            cardHTMLCollection[i].style.left = `calc(50% + 25px - ${(card_position[i]*-1)}px)`
+            //console.log(cardHTMLCollection[i].style.left)
         }
 
-        for(let i=midRight + 1; i<card_list.length; i++){
-            card_list[i].style.left = `calc(50% + 25px + ${card_position[i]}px)`
-            //console.log(card_list[i].style.left)
+        for(let i=midRight ; i<cardHTMLCollection.length; i++){
+            if(i==midPointer){
+                cardHTMLCollection[i].style.filter = `drop-shadow(0px 4px 4px rgba(0, 0, 0, 0.25))`
+                cardHTMLCollection[i].style.transform = 'scale(1)'
+            }
+            cardHTMLCollection[i].style.left = `calc(50% + 25px + ${card_position[i]}px)`
+            //console.log(cardHTMLCollection[i].style.left)
         }
 
         
-        console.log(card_list[0].style.left)
-        console.log(card_list[1].style.left)
-        console.log(card_list[2].style.left)
-        console.log(card_list[3].style.left)
+        console.log(cardHTMLCollection[0].style.left)
+        console.log(cardHTMLCollection[1].style.left)
+        console.log(cardHTMLCollection[2].style.left)
+        console.log(cardHTMLCollection[3].style.left)
 
         
         console.log(card_position)
@@ -110,7 +135,8 @@ function main(){
         
 
     }else{
-        let mid = parseInt(card_list.length/2)
+        let mid = parseInt(cardHTMLCollection.length/2)
+        midPointer = mid
         card_position.push(0)
         
         for(let i=0; i<mid; i++){
@@ -119,13 +145,17 @@ function main(){
         }
 
         counter = 1;
-        for(let i=mid+1; i<card_list.length; i++){
+        for(let i=mid+1; i<cardHTMLCollection.length; i++){
             card_position.push(315*(counter))
             counter++;
         }
 
-        for(let i=0; i<card_list.length; i++){
-            card_list[i].style.left = `calc(50% - 132px + ${card_position[i]}px)`
+        for(let i=0; i<cardHTMLCollection.length; i++){
+            if(i==midPointer){
+                cardHTMLCollection[i].style.filter = `drop-shadow(0px 4px 4px rgba(0, 0, 0, 0.25))`
+                cardHTMLCollection[i].style.transform = 'scale(1)'
+            }
+            cardHTMLCollection[i].style.left = `calc(50% - 132px + ${card_position[i]}px)`
         }
     }
 
